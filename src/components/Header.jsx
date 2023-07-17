3df8344e95e683cc19fe9edd6e323560962d2327
victor9889 <victorrzna@gmail.com>
2023-07-17 12:37:48 +0200

    Menu ahora es responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/estilos.css';
 import '../styles/normalize.css';
@@ -6,37 +6,68 @@ import '../styles/normalize.css';
 import Menu from './Menu';
 
 const Header = () => {
-    
+    const [abierto, setAbierto] = useState(false);
+    const toggleMenu = () => {
+        setAbierto(!abierto);
+    };
+    const cerrarMenu = () => {
+        setAbierto(false);
+    };
     return (
-      
         <header className="hero h-full w-full">
-            <nav className="nav container">
-                <div className="nav__logo">
-                    <h2 className="nav__title">Santa Catalina</h2>
+            <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <div className="nav__logo">
+                        <h2 className="nav__title">Santa Catalina</h2>
+                    </div>
                 </div>
-
-                <ul className="nav__link nav__link--menu">
-                    <li className="nav__items">
-                        <Link to="/" className="nav__links hover:text-sky-400">Inicio</Link>
-                    </li>
-                    <li className="nav__items">
-                        <Link to="/about" className="nav__links hover:text-sky-400">Acerca de</Link>
-                    </li>
-                    <li className="nav__items">
-                        <Link to="/contact" className="nav__links hover:text-sky-400">Contacto</Link>
-                    </li>
-                    <li className="nav__items">
-                        <Link to="/about" className="nav__links hover:text-sky-400">Paciente</Link>
-                    </li>
-
-                    <img type="image/svg+xml" src="/imagenes/close.svg" className="nav__close"/>
-                </ul>
-
-                <div className="nav__menu">
-                    <img type="image/svg+xml" src="/imagenes/menu.svg" className="nav__img"/>
+                <div className="flex lg:hidden">
+                    <button onClick={toggleMenu} type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+                        <span className="sr-only">Open main menu</span>
+                        <img type="image/svg+xml" src="/imagenes/menu.svg" className="nav__img" />
+                    </button>
                 </div>
-                
+                <div className={`hidden lg:flex lg:gap-x-12 ${abierto ? 'flex' : ''}`}>
+                    <ul className="nav__link nav__link--menu">
+                        <li className="nav__items">
+                            <Link to="/" className="nav__links hover:text-sky-400">Inicio</Link>
+                        </li>
+                        <li className="nav__items">
+                            <Link to="/about" className="nav__links hover:text-sky-400">Acerca de</Link>
+                        </li>
+                        <li className="nav__items">
+                            <Link to="/contact" className="nav__links hover:text-sky-400">Contacto</Link>
+                        </li>
+                        <li className="nav__items">
+                            <Link to="/about" className="nav__links hover:text-sky-400">Paciente</Link>
+                        </li>
+
+                        <img type="image/svg+xml" src="/imagenes/close.svg" className="nav__close" />
+                    </ul>
+                </div>
             </nav>
+            <div className={`lg:hidden ${abierto ? 'block' : 'hidden'}`} role="dialog" aria-modal="true">
+                <div className="fixed inset-0 z-50" onClick={cerrarMenu}></div>
+                <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={cerrarMenu}>
+                            <span className="sr-only">Close menu</span>
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Inicio</Link>
+                                <Link to="/about" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Acerca de</Link>
+                                <Link to="/contact" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contacto</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <section className="hero__container container">
                 <h1 className="hero__title">Dedicados a tu bienestar en cada paso del camino</h1>
