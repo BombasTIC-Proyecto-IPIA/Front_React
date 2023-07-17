b1575b293a8341e1ba90d18c1efd3365961644f6
Luisa <lilo_va@hotmail.com>
2023-07-17 13:49:12 +0200

    Vista Paciente

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './components/Home'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/LogIn';
+import VistaPacientes from './components/VistaPacientes';
 import ListaPacientes from './components/ListaPacientes';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import VistaDoctores from './components/VistaDoctores';
@@ -15,8 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login/>}/>
-          <Route path="/pacientes" element={<ListaPacientes/>}/>
           <Route path="/doctor" element={<VistaDoctores/>}/>
+          <Route path="/pacientes" element={<VistaPacientes/>}/>
         </Routes>
       </main>
       <Footer />
---
+++ src/components/VistaPacientes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+
+const VistaPacientes = () => {
+  // const user = useSelector(state => state.user);
+    return(
+        <div className="container about">
+          <h2 className="text-left text-3xl mb-20" >Bienvenido/a </h2> <br />
+          <h3 className="text-2xl mb-10 font-style: italic" > Tus diagnosticos</h3>
+          
+        </div>
+
+    )
+}
+
+export default VistaPacientes;
