ff6234305a69f2a9e99a4947492f787eb4c5c08e
victor9889 <victorrzna@gmail.com>
2023-07-19 12:55:32 +0200

    oush

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from "./redux/store"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from "./redux/store"; // Import both store and persistor
 import { Provider } from 'react-redux';
-//Cambios
+import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}> {/* Wrap your App component with PersistGate */}
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
-)
+);
