d2c9a6cf5c3d9592eeb26289cf64a50f25aab9c9
victor9889 <victorrzna@gmail.com>
2023-07-20 13:28:04 +0200

    push

--- src/components/RealizarDiagnostico.jsx
+++ src/components/RealizarDiagnostico.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import ImageDrop from './ImageDragger';
 
 const RealizarDiagnostico = () => {
-    const handleFileDrop = async (file) => {
-        try {
-            const folderHandle = await window.showDirectoryPicker();
-            const directoryHandle = await folderHandle.getDirectoryHandle('imagenesDiagnostico', { create: true });
-            const fileHandle = await directoryHandle.getFileHandle(file.name, { create: true });
-            const writable = await fileHandle.createWritable();
-            await writable.write(file);
-            await writable.close();
-            console.log('File saved successfully!');
-        } catch (error) {
-            console.error('Error saving the file:', error);
-        }
+    const handleFileDrop = (file) => {
+        // Crear una instancia de FormData para enviar el archivo al servidor
+        const formData = new FormData();
+        formData.append('file', file);
+
+        // Enviar el archivo al servidor
+        fetch('http://localhost:3000/utilities/executeModel', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((response) => response.text())
+            .then((result) => {
+                console.log(result); // Mensaje de respuesta del servidor
+            })
+            .catch((error) => {
+                console.error('Error al subir el archivo:', error);
+            });
     };
 
 
