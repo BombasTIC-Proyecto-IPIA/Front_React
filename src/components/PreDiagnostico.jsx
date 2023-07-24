7d79e0c35c596b67e75c0eda2d43bdac9023f9ec
victor9889 <victorrzna@gmail.com>
2023-07-24 10:11:53 +0200

    push 😸

--- src/components/PreDiagnostico.jsx
+++ src/components/PreDiagnostico.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageDrop from "./ImageDragger";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"; // Import useNavigate
 
 const PreDiagnostico = () => {
     const { dni } = useParams();
+    const navigate = useNavigate(); // Use the useNavigate hook here
     const [resultado, setResultado] = useState("");
-    const [convertedImageBlob, setConvertedImageBlob] = useState(null);
+    const [imageData, setImageData] = useState(null);
+
+    useEffect(() => {
+        if (resultado !== "") {
+            console.log(resultado.toString());
+            // Use navigate to go to the DiagnosisResult component with the necessary data
+            navigate(`/diagnostico`, { state: { resultado, imageData: imageData, pacienteDNI: dni } });
+        }
+    }, [resultado, imageData, dni, navigate]);
 
     const handleFileDrop = (file) => {
         const formData = new FormData();
@@ -23,15 +32,14 @@ const PreDiagnostico = () => {
             })
             .then((result) => {
                 if (result.status === "OK") {
+                     // Set the image data in state
+                     setImageData(result.data.imagen);
                     if (result.data.resultado.trim() === "1") {
+                        console.log("Hola");
                         setResultado(true);
                     } else {
                         setResultado(false);
                     }
-
-                    // Convert the base64 string back to an image blob
-                    const imageBlob = b64toBlob(result.data.imagen, 'image/png');
-                    setConvertedImageBlob(imageBlob);
                 }
             })
             .catch((error) => {
@@ -39,38 +47,9 @@ const PreDiagnostico = () => {
             });
     };
 
-    function b64toBlob(base64, contentType = '', sliceSize = 512) {
-        const byteCharacters = atob(base64);
-        const byteArrays = [];
-
-        for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-            const slice = byteCharacters.slice(offset, offset + sliceSize);
-            const byteNumbers = new Array(slice.length);
-            for (let i = 0; i < slice.length; i++) {
-                byteNumbers[i] = slice.charCodeAt(i);
-            }
-            const byteArray = new Uint8Array(byteNumbers);
-            byteArrays.push(byteArray);
-        }
-
-        const blob = new Blob(byteArrays, { type: contentType });
-        return blob;
-    }
-
     return (
         <div>
             <ImageDrop onFileDrop={handleFileDrop} />
-            {convertedImageBlob && (
-                <div>
-                    <h2>Resultado</h2>
-                    {resultado ? <p>Resultado positivo</p> : <p>Resultado negativo</p>}
-                    <h2>Imagen Convertida</h2>
-                    <img
-                        src={URL.createObjectURL(convertedImageBlob)}
-                        alt="Imagen Convertida"
-                    />
-                </div>
-            )}
         </div>
     );
 };
